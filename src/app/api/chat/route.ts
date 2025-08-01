a1581ebcfd1edcd49f543882a8a7cc3c5f9d568a
muralikrishna2977 <muralikrishna1502887@gmail.com>
2025-08-01 18:10:21 +0530

    key fix

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,10 +1,6 @@
 import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
 export const runtime = "edge";
 
 export async function POST(req: Request) {
@@ -17,6 +13,10 @@ export async function POST(req: Request) {
     frequency_penalty,
     presence_penalty,
   } = await req.json();
+
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY!,
+  });
 
   const response = await openai.chat.completions.create({
     stream: true,
